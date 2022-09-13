faf53837261fc8c9fc4c755e3a995c076a22505a
banxia0v0 <1106624418@qq.com>
2022-09-13 11:30:50 +0800

    hello ljh

---
+++ 01hello.py
@@ -0,0 +1,38 @@
+
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-  
+#====#====#====#====   
+#Author:
+#CreatDate:
+#Version: 
+#====#====#====#====
+
+#单行注释,解析器看到你的注释,是不会运行你的注释
+
+#下面的代码表示打印hello在控制台上
+print("hello")
+print("world")
+
+
+
+'''
+多行注释
+xxxx
+xxxx
+xxxx
+'''
+
+"""
+多行注释
+xxxx
+xxxx
+xxxx
+"""
+#注释的快捷键:ctr+/
+
+# xxx
+# xxx
+# xxx
+
+
+
